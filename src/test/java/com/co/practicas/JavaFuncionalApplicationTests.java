f20804acbef6866e64842cfb1df22d1ebef1ddd7
Jofre Dev <gavidiajofreeduardo@gmail.com>
2023-09-20 11:02:56 -0500

    second commit

---
+++ src/test/java/com/co/practicas/JavaFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFuncionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
